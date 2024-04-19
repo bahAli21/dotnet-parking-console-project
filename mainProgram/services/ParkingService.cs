32f315b11eebf5ef1b2abf2bf0245b631db0b4d7
Mamadou BAH <bah311802@gmail.com>
2024-04-19 23:18:00 +0200

    Implementation de la class qui gere les places de parking

---
+++ mainProgram/services/ParkingService.cs
@@ -0,0 +1,48 @@
+using System;
+using models;
+namespace services
+{
+    public class ParkingService
+    {
+       public List<ParkingSpot> ListParkingSpots { get; set; }
+       ParkingService() => ListParkingSpots = new List<ParkingSpot>();
+
+       public void AddParkingSpot(ParkingSpot parkingSpot)
+       {
+            ListParkingSpots.Add(parkingSpot);
+       }
+
+       public void RemoveParkingSpot(ParkingSpot parkingSpot)
+       {
+            ListParkingSpots.Remove(parkingSpot);
+       }
+
+       public List<ParkingSpot> GetAvailableParkingSpots() 
+       {
+            List<ParkingSpot> availableSpot = new List<ParkingSpot> ();
+            for (int i = 0; i < ListParkingSpots.Count; i++)
+            {
+                if (!ListParkingSpots[i].Status)
+                    availableSpot.Add(ListParkingSpots[i]);
+            }
+            
+            return availableSpot; 
+       }
+
+        public List<ParkingSpot> GetOccupiedParkingSpots()
+        {
+            List<ParkingSpot> occupiedSpot = new List<ParkingSpot>();
+            for (int i = 0; i < ListParkingSpots.Count; i++)
+            {
+                if (ListParkingSpots[i].Status)
+                    occupiedSpot.Add(ListParkingSpots[i]);
+            }
+
+            return occupiedSpot;
+        }
+
+
+
+
+    }
+}
