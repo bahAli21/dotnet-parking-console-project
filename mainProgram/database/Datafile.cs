5ad7e9c1af5f4cb6961d3fdf8ece58dbb39f7856
Mamadou BAH <bah311802@gmail.com>
2024-04-21 02:17:31 +0200

    J'ameliore les les methodes de recuperation et d'ajout a la base de donné

--- mainProgram/database/Datafile.cs
+++ mainProgram/database/Datafile.cs
@@ -8,42 +8,51 @@ namespace database
 {
     public static class Database
     {
-        public static string[][] GetData(string filepath)
-        {
-            // List<Dictionary<string, string>> data = new List<Dictionary<string, string>>();
 
-            string[][] data = {};
+        public static void GetData(string filepath, ref string[][] data)
+        {
+            
+
             try
             {
-                Trace.Assert(filepath != "");
-                int i = 0;
-
                 if (File.Exists(filepath))
                 {
+                    List<string[]> dataList = new List<string[]>();
+
                     using (StreamReader sr = new StreamReader(filepath))
                     {
-                        string tmpLine = string.Empty;
+                        string tmpLine;
                         while ((tmpLine = sr.ReadLine()) != null)
                         {
-                            data[i] = tmpLine.Split(" ");
-                            i++;
+                            // Vérifie si la ligne n'est pas vide ou nulle
+                            if (!string.IsNullOrEmpty(tmpLine))
+                            {
+                                string[] lineData = tmpLine.Split(' ');
+                                dataList.Add(lineData);
+                            }
                         }
                     }
 
+                    // Convertir la liste en tableau 2D
+                    data = dataList.ToArray();
                 }
                 else
-                    throw new Exception("Erreur Lors de l'ouverture du fichier");
-
-            }catch (Exception e)
+                {
+                    throw new FileNotFoundException("Fichier non trouvé : " + filepath);
+                }
+            }
+            catch (Exception e)
             {
-                Console.WriteLine(e.Message);
+                Console.WriteLine("Une erreur s'est produite : " + e.Message);
             }
 
-            return data;
         }
+
 
         public static void SetData(string filepath, string[] lineSplit)
         {
+           
+            
             string line = string.Join(" ", lineSplit);
 
             //true parcequ'on souhaite ajouter une nouvelle ligne sans ecraser le contenu ancien
@@ -51,6 +60,7 @@ namespace database
             {
                 if (File.Exists(filepath))
                 {
+                    
                     using (StreamWriter writer = new StreamWriter(filepath, true))
                     {
                         writer.WriteLine(line);
